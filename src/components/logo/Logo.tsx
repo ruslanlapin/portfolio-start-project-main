9bb64b7e36756fe12c4b49c8eda6bf915050d5bb
Ruslan <ruslanlapin@yandex.ru>
2024-03-05 11:57:22 +0300

    Commit-1 Header Icon

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Icon} from "../icon/Icon";
 
 export const Logo = () => {
     return (
         <a href="">
-            <img src="" alt=""/>
+            <Icon iconId={'code'}/>
         </a>
     );
 };
