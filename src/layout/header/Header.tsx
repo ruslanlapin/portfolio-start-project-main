7fa37e2391bc34fea14e3680d90995a8d990989e
Ruslan <ruslanlapin@yandex.ru>
2024-04-13 20:48:34 +0300

    Commit-3 Header part 3

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -4,6 +4,7 @@ import {Logo} from "../../components/logo/Logo";
 import {Container} from "../../components/Container";
 import {FlexWrapper} from "../../components/FlexWrapper";
 import {HeaderMenu} from "./headerMenu/HeaderMenu";
+import {MobileMenu} from "./mobileMenu/MobileMenu";
 
 const items =["Home", "Skills", "Works", "Testimony", "Contact"]
 
@@ -14,6 +15,7 @@ export const Header = () => {
                 <FlexWrapper justify={"space-between"} align={"center"}>
                     <Logo/>
                     <HeaderMenu menuItems={items}/>
+                    <MobileMenu menuItems={items}/>
                 </FlexWrapper>
             </Container>
         </StyledHeader>
--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -9,5 +9,10 @@ export const theme = {
 
         borderColor: "#4A4A4A",
         placeholderColor: "#495057"
+    },
+
+    media: {
+        tablet: "screen and (max-width: 768px)",
+        mobile: "screen and (max-width: 576px)",
     }
 }
