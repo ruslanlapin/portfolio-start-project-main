5b3eec139fab965fb9eaa51c9151281b05ee8020
Ruslan <ruslanlapin@yandex.ru>
2024-04-16 17:28:30 +0300

    Commit-3 Contacts Slogan Footer part 3

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import {Icon} from "../../components/icon/Icon";
 import {FlexWrapper} from "../../components/FlexWrapper";
 import {theme} from "../../styles/Theme";
+import {font} from "../../styles/Common";
 
 export const Footer = () => {
     return (
@@ -43,9 +44,7 @@ const StyledFooter = styled.footer`
     padding: 40px 0;
 `
 const Name = styled.span`
-    font-family: "Josefin Sans", sans-serif;
-    font-size: 22px;
-    font-weight: 700;
+    ${font({family: "'Josefin Sans', sans-serif", weight: 700, Fmax: 22, Fmin: 16})}
     letter-spacing: 3px;
 `
 const SocialList = styled.ul`
