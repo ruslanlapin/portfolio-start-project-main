3c5136ee6c73b16f131c8ef41f31b99d7e557e3e
Ruslan <ruslanlapin@yandex.ru>
2024-04-17 11:51:21 +0300

    Commit-3 Header part 3 (BurgerButton {false})

--- src/layout/header/mobileMenu/MobileMenu.tsx
+++ src/layout/header/mobileMenu/MobileMenu.tsx
@@ -5,11 +5,11 @@ import {theme} from "../../../styles/Theme";
 export const MobileMenu = (props: { menuItems: Array<string> }) => {
     return (
         <StyledMobileMenu>
-            <BurgerButton isOpen={true}>
+            <BurgerButton isOpen={false}>
                 <span></span>
             </BurgerButton>
 
-            <MobileMenuPopup isOpen={true}>
+            <MobileMenuPopup isOpen={false}>
                 <ul>
                     {props.menuItems.map((item, index) => {
                         return <ListItem key={index}>
